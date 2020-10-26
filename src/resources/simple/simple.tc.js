7e7120fc636dfff513b3fa3068a0db85d03a5c4b
romantic <cesc_fabregas1414@hotmail.com>
2020-10-26 21:43:29 +0700

    resolver simple add how to create type

--- src/resources/simple/simple.tc.js
+++ src/resources/simple/simple.tc.js
@@ -2,7 +2,26 @@ import { schemaComposer } from 'graphql-compose'
 import composeWithJson from 'graphql-compose-json'
 
 export default function () {
+  // ## 1 compose with json
   const TC = composeWithJson('Simple', { id: '', description: '' })
+
+  // ## 2
+  // Type creation https://graphql-compose.github.io/docs/basics/understanding-types.html
+  // const TC = schemaComposer.createObjectTC({
+  //   name: 'Simple',
+  //   fields: {
+  //     id: 'String',
+  //     description: 'String',
+  //   },
+  // })
+
+  // ## 3 using SDL for definition new ObjectType
+  // const TC = `type Simple {
+  //   id: String
+  //   description: String
+  // }`
+
+  // ## 4 using standard GraphQL Type
 
   schemaComposer.Query.addFields({
     SimpleQuery: {
